b57964490310ff1d5484a6876fccde156cc486c0
zhouzhouthezhou <kylezhou2222@gmail.com>
2019-09-18 12:12:52 -0600

    blink

---
+++ blink/main.c
@@ -0,0 +1,20 @@
+#include "msp.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	P1DIR |= 0x01;
+	P1OUT &= 0x00;
+	int i;
+	while(1){
+	    P1OUT |= 0x01;
+	    for(i = 0; i < 100000; i++);
+	    P1OUT &= 0x00;
+	    for(i = 0; i < 100000; i++);
+	}
+}
